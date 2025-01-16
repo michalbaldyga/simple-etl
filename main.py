000d64f272c4347c888047034ddc6ce4867e6059
michalbaldyga <michal.baldyga01@gmail.com>
2025-01-16 15:58:03 +0100

    SE-4 - test product category

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from src.extract import get_users, get_user_carts
+from src.extract import get_users, get_user_carts, get_product_category
 
 
 def main():
     #r = get_users(10, 0, ['age'])
     r = get_user_carts(6)
+    print(r)
+    r = get_product_category(108)
     print(r)
 
 
