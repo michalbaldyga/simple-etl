248df083123894dcc315e44604aa12dae894b1ee
michalbaldyga <michal.baldyga01@gmail.com>
2025-01-16 15:13:26 +0100

    SE-3 - test user carts request

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from src.extract import get_users
+from src.extract import get_users, get_user_carts
 
 
 def main():
-    r = get_users(10, 0, ['age'])
+    #r = get_users(10, 0, ['age'])
+    r = get_user_carts(6)
     print(r)
 
 
