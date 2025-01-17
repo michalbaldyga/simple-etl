d432831176a8d0fb6e3cf5f4c8d6d7e8255e93f9
michalbaldyga <michal.baldyga01@gmail.com>
2025-01-17 12:58:49 +0100

    SE-6 - test grouping

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
-from src.extract import get_users, get_user_carts, get_product_category
+from src.extract import get_users, get_user_carts, get_product_category, \
+    get_user_country
+
+from src.transform import extract_products_from_carts, group_products_by_category
 
 
 def main():
-    #r = get_users(10, 0, ['age'])
-    r = get_user_carts(6)
-    print(r)
-    r = get_product_category(108)
-    print(r)
+    user_params = ["firstName", "lastName", "age", "gender", "address"]
+    users = get_users(1, 5, user_params)
+    carts = get_user_carts(6).get('carts')
+    carts = [carts[0], carts[0]]
+    # r = get_product_category(108)
+    print(users)
+    print(carts)
 
+    products = extract_products_from_carts(carts)
+    print(group_products_by_category(products))
+
+    # get_user_country(52.509669, 13.376294)
 
 if __name__ == "__main__":
     main()
