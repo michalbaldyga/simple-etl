3f21c2495b4a0f57b26399afe08553413e0507ae
michalbaldyga <michal.baldyga01@gmail.com>
2025-01-20 16:46:08 +0100

    SE-10 - simplify code

--- main.py
+++ main.py
@@ -5,13 +5,9 @@ from src.transform import get_user_data
 
 def main():
     user_params = ["firstName", "lastName", "age", "gender", "address"]
-    users = get_users(2, 0, user_params).get('users')
-    results = []
-    for user in users:
-        results.append(get_user_data(user))
-
+    users = get_users(30, 0, user_params)
+    results = [get_user_data(user) for user in users]
     save_to_db(results)
-
 
 if __name__ == "__main__":
     main()
