a94d5bffe817591e34e3830d53a6aaaf8511a2a2
michalbaldyga <michal.baldyga01@gmail.com>
2025-01-20 13:16:38 +0100

    SE-7 - adjust main func

--- main.py
+++ main.py
@@ -1,25 +1,15 @@
-from src.extract import get_user_carts, get_users
+from src.extract import get_user_carts, get_users, get_user_country
 from src.transform import (
     extract_products_from_carts,
-    group_products_by_category, get_most_common_category,
+    group_products_by_category, get_most_common_category, get_user_data,
 )
 
 
 def main():
     user_params = ["firstName", "lastName", "age", "gender", "address"]
-    users = get_users(1, 5, user_params)
-    carts = get_user_carts(6).get('carts')
-    carts = [carts[0], carts[0]]
-    # r = get_product_category(108)
-    print(users)
-    print(carts)
-
-    products = extract_products_from_carts(carts)
-    products_gr = group_products_by_category(products)
-    products_gr['test'] = 20
-    print(products_gr)
-    print(get_most_common_category(products_gr))
-    # get_user_country(52.509669, 13.376294)
+    users = get_users(10, 0, user_params).get('users')
+    for user in users:
+        print(get_user_data(user))
 
 if __name__ == "__main__":
     main()
