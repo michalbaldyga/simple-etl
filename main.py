f744fd3cb50c825882a25a9228cd4cca9727395e
michalbaldyga <michal.baldyga01@gmail.com>
2025-01-21 13:02:36 +0100

    SE-11 - add save to file to main flow

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import logging.config
 
 from config.config import LOG_FILE_PATH, LOGGING_CONFIG_FILE
 from src.extract import get_users
-from src.load import save_to_db
+from src.load import save_to_db, save_to_file
 from src.transform import get_user_data
 
 logging.config.fileConfig(
@@ -14,7 +14,7 @@ logger = logging.getLogger("logger_file")
 
 def main():
     user_params = ["firstName", "lastName", "age", "gender", "address"]
-    batch_size = 30
+    batch_size = 2
     offset = 0
 
     while True:
@@ -23,6 +23,7 @@ def main():
             if not users:
                 break
             results = [get_user_data(user) for user in users]
+            save_to_file(results)
             save_to_db(results)
             offset += batch_size
         except Exception as exc:
