69127bc7de1480c0dd21c318ee2d4ecbdc17d080
michalbaldyga <michal.baldyga01@gmail.com>
2025-01-22 13:44:26 +0100

    SE-13 - add new path

--- config/config.py
+++ config/config.py
@@ -2,6 +2,7 @@ USERS_ENDPOINT = "https://dummyjson.com/users"
 USER_CARTS_ENDPOINT = "https://dummyjson.com/carts/user"
 PRODUCTS_ENDPOINT = "https://dummyjson.com/products"
 
-DATABASE = "data/database/shop.db"
 LOG_FILE_PATH = "logs/app.log"
 LOGGING_CONFIG_FILE = "config/logging.conf"
+DATABASE_PATH = "data/database/shop.db"
+CSV_FILE_PATH = "data/file/users.csv"
