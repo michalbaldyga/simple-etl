eb65f5ac8fe585f56291b852f8329b968249c89c
michalbaldyga <michal.baldyga01@gmail.com>
2025-01-23 09:57:26 +0100

    SE-13 - adjust main flow to new struct

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import logging.config
 
-from config.config import LOG_FILE_PATH, LOGGING_CONFIG_FILE
+from config.config import DATABASE_PATH, LOG_FILE_PATH, LOGGING_CONFIG_FILE
 from src.extract import get_users
-from src.load import save_to_db, save_to_file
+from src.load import (
+    close_db_connection,
+    create_users_table,
+    insert_into_users,
+    open_db_connection,
+)
 from src.transform import get_user_data
 
 logging.config.fileConfig(
@@ -14,20 +19,32 @@ logger = logging.getLogger("logger_file")
 
 def main():
     user_params = ["firstName", "lastName", "age", "gender", "address"]
-    batch_size = 2
+    batch_size = 1
     offset = 0
+    conn = None
 
     while True:
         try:
             users = get_users(batch_size, offset, user_params)
             if not users:
                 break
-            results = [get_user_data(user) for user in users]
-            save_to_file(results)
-            save_to_db(results)
+
+            users_clean = [get_user_data(user) for user in users]
+
+            if not conn:
+                conn = open_db_connection(DATABASE_PATH)
+
+            create_users_table(conn)
+            insert_into_users(conn, users_clean)
             offset += batch_size
+
         except Exception as exc:
             logger.error(f"An unexpected error occurred: {exc}")
+
+        finally:
+            close_db_connection(conn)
+
+        break
 
 
 if __name__ == "__main__":
